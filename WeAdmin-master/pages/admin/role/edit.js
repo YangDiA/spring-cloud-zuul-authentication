14558bd2c6576724f62b3511be80ad0a788b86b5
Linrry.yang <linrry.yang@shoptps.com>
2018-04-26 18:37:55 +0800

    test 未完成

--- WeAdmin-master/pages/admin/role/edit.js
+++ WeAdmin-master/pages/admin/role/edit.js
@@ -1,18 +1,21 @@
 layui.extend({
-    admin: '{/}../../../static/js/admin'
+    admin: '{/}../../../static/js/admin',
+    laybind: '{/}../../../static/js/laybind'
 });
 
-layui.use(['table', 'jquery', 'admin','laydate'], function() {
+layui.use(['table', 'jquery', 'admin','laybind','laydate'], function() {
     var table = layui.table,
         $ = layui.jquery,
         jQuery=layui.jquery,
         admin = layui.admin,
+        laybind= layui.laybind,
         laydate = layui.laydate;
 
-
-    var initData = {roleName:'张三'};
-    var bind = new laybind($('#bind'), {data:initData});
-
+    var bind =  laybinddata($('#bind'),{data:{name:"ddd"}});
+    $('#tijiao').on('click', function() {
+        var data = bind.getData();
+        console.log(data)
+    });
 
 
 
--- WeAdmin-master/static/js/bodyTab.js
+++ WeAdmin-master/static/js/bodyTab.js
@@ -1,9 +1,4 @@
-/*
-	@Author: 请叫我马哥
-	@Time: 2017-04
-	@Tittle: tab
-	@Description: 点击对应按钮添加新窗口
-*/
+
 var tabFilter,menu=[],liIndex,curNav,delMenu;
 layui.define(["element","jquery","layer"],function(exports){
 
